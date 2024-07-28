40b1277f95049e01305ce5b3cd4f0f2d5acd5a6b
BitPulse01 <bitpulse0101@gmail.com>
2024-07-28 20:46:28 +0330

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,7 +1,7 @@
-API_KEY = "7268606074:AAEv5qlcJglc4uCbQPJxsUw6WL_XZ-27rLE"
+API_KEY = "YOUR API TOKEN"
 
 config = {
     "user":"root",
-    "password":"Thisismysqlpass1",
+    "password":"YOUR MYSQL ROOT ACCOUNT PASSOWRD",
     "host":"localhost"
 }
